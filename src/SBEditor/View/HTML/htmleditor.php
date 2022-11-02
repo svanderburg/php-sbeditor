b0dfcaf68c3b7580206fc81426bfc65695640dbd
Sander van der Burg <svanderburg@gmail.com>
2022-11-02 01:28:23 +0100

    Use short echo tags

--- src/SBEditor/View/HTML/htmleditor.php
+++ src/SBEditor/View/HTML/htmleditor.php
@@ -21,9 +21,9 @@ namespace SBEditor\View\HTML;
 function displayHTMLEditor(string $id, string $name, string $iframePage, string $iconsPath, string $contents = NULL, int $width = 60, int $height = 20): void
 {
 	?>
-	<div class="sbeditor" id="<?php print($id); ?>">
+	<div class="sbeditor" id="<?= $id ?>">
 		<div style="display: none;">
-			<select onchange="sbeditor.doRichEditCommand('<?php print($id); ?>', 'formatBlock', this.options[this.selectedIndex].value);">
+			<select onchange="sbeditor.doRichEditCommand('<?= $id ?>', 'formatBlock', this.options[this.selectedIndex].value);">
 				<option value="">[Style]</option>
 				<option value="<p>">Paragraph</option>
 				<option value="<h1>">Heading 1</option>
@@ -32,22 +32,22 @@ function displayHTMLEditor(string $id, string $name, string $iframePage, string 
 				<option value="<blockquote>">Blockquote</option>
 				<option value="<pre>">Preformatted</option>
 			</select><br>
-			<button onclick="sbeditor.doRichEditCommand('<?php print($id); ?>', 'bold'); return false;"><strong>Highlight</strong></button>
-			<button onclick="sbeditor.doRichEditCommand('<?php print($id); ?>', 'subscript'); return false;">S<sub>ubscript</sub></button>
-			<button onclick="sbeditor.doRichEditCommand('<?php print($id); ?>', 'superscript'); return false;">S<sup>uperscript</sup></button>&nbsp;|&nbsp;
-			<button onclick="sbeditor.addLink('<?php print($id); ?>'); return false;"><img src="<?php print($iconsPath); ?>/a.gif" alt="Add link"></button>
-			<button onclick="sbeditor.addImage('<?php print($id); ?>'); return false;"><img src="<?php print($iconsPath); ?>/img.gif" alt="Add image"></button>&nbsp;|&nbsp;
-			<button onclick="sbeditor.doRichEditCommand('<?php print($id); ?>', 'InsertUnorderedList'); return false;"><img src="<?php print($iconsPath); ?>/ul.gif" alt="Add unordered list"></button>
-			<button onclick="sbeditor.doRichEditCommand('<?php print($id); ?>', 'InsertOrderedList'); return false;"><img src="<?php print($iconsPath); ?>/ol.gif" alt="Add ordered list"></button>&nbsp;|&nbsp;
-			<button onclick="sbeditor.addTableWithHorizontalHeader('<?php print($id); ?>'); return false;"><img src="<?php print($iconsPath); ?>/table_hh.gif" alt="Add table with horizontal header"></button>
-			<button onclick="sbeditor.addTableWithVerticalHeader('<?php print($id); ?>'); return false;"><img src="<?php print($iconsPath); ?>/table_vh.gif" alt="Add table with vertical header"></button>
-			<button onclick="sbeditor.addTableWithoutBorder('<?php print($id); ?>'); return false;"><img src="<?php print($iconsPath); ?>/table_nb.gif" alt="Add table without border"></button>&nbsp;|&nbsp;<br>
-			<iframe src="<?php print($iframePage); ?>" style="<?php print("width: ".$width."em; height: ".$height."em;"); ?>"></iframe>
+			<button onclick="sbeditor.doRichEditCommand('<?= $id ?>', 'bold'); return false;"><strong>Highlight</strong></button>
+			<button onclick="sbeditor.doRichEditCommand('<?= $id ?>', 'subscript'); return false;">S<sub>ubscript</sub></button>
+			<button onclick="sbeditor.doRichEditCommand('<?= $id ?>', 'superscript'); return false;">S<sup>uperscript</sup></button>&nbsp;|&nbsp;
+			<button onclick="sbeditor.addLink('<?= $id ?>'); return false;"><img src="<?= $iconsPath ?>/a.gif" alt="Add link"></button>
+			<button onclick="sbeditor.addImage('<?= $id ?>'); return false;"><img src="<?= $iconsPath ?>/img.gif" alt="Add image"></button>&nbsp;|&nbsp;
+			<button onclick="sbeditor.doRichEditCommand('<?= $id ?>', 'InsertUnorderedList'); return false;"><img src="<?= $iconsPath ?>/ul.gif" alt="Add unordered list"></button>
+			<button onclick="sbeditor.doRichEditCommand('<?= $id ?>', 'InsertOrderedList'); return false;"><img src="<?= $iconsPath ?>/ol.gif" alt="Add ordered list"></button>&nbsp;|&nbsp;
+			<button onclick="sbeditor.addTableWithHorizontalHeader('<?= $id ?>'); return false;"><img src="<?= $iconsPath ?>/table_hh.gif" alt="Add table with horizontal header"></button>
+			<button onclick="sbeditor.addTableWithVerticalHeader('<?= $id ?>'); return false;"><img src="<?= $iconsPath ?>/table_vh.gif" alt="Add table with vertical header"></button>
+			<button onclick="sbeditor.addTableWithoutBorder('<?= $id ?>'); return false;"><img src="<?= $iconsPath ?>/table_nb.gif" alt="Add table without border"></button>&nbsp;|&nbsp;<br>
+			<iframe src="<?= $iframePage ?>" style="<?= "width: ".$width."em; height: ".$height."em;" ?>"></iframe>
 		</div>
 		<div>
-			<textarea name="<?php print($name); ?>" cols="<?php print($width); ?>" rows="<?php print($height); ?>"><?php if($contents != NULL) print(htmlentities($contents)); ?></textarea>
+			<textarea name="<?= $name ?>" cols="<?= $width ?>" rows="<?= $height ?>"><?php if($contents != NULL) print(htmlentities($contents)); ?></textarea>
 		</div>
-		<input type="checkbox" onclick="sbeditor.toggleView('<?php print($id); ?>', this.checked);" checked>View source
+		<input type="checkbox" onclick="sbeditor.toggleView('<?= $id ?>', this.checked);" checked>View source
 	</div>
 	<?php
 }
